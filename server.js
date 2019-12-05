ad7bc49541f4b723b8321549e685717b0712efff
Jax Clark <jamaecla@gmail.com>
2019-12-05 12:16:36 -0700

    tweaking

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const path = require('path')
+const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
-mongoose.connect('mongodb://localhost:27017/cluedb',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/cluedb',
     {
         useNewUrlParser: true,
         useFindAndModify: false,
@@ -19,6 +22,9 @@ mongoose.connect('mongodb://localhost:27017/cluedb',
     app.use("/character", require("./routes/CharacterRouter"))
     app.use("/weapon", require("./routes/WeaponRouter"))
 
-    app.listen(7000, () => {
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    })
+    app.listen(PORT, () => {
         console.log('server is running')
     })
